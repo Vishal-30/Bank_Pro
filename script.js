95c5fd66a6cc7595d0f2a87f22a6c05b6ea2e3d5
Vishal <prattipativishal@gmail.com>
2021-07-08 20:43:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -120,17 +120,3 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
